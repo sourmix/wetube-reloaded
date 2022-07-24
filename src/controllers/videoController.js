4fa735ac5ce8715bb2fc494f90db664ff6d25ebb
sourmix <75124027+sourmix@users.noreply.github.com>
2022-07-24 21:36:19 +0900

    pug1

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,9 +1,6 @@
-export const trending = (req, res) => res.send("Trending Videos");
-export const see = (req, res) => {
-  console.log(req.params);
-  res.send(`See Videos # ${req.params.id}`);
-};
-export const edit = (req, res) => res.send("edit video");
+export const trending = (req, res) => res.render("home", { pageTitle: "Home" });
+export const see = (req, res) => res.render("watch", { pageTitle: "Watch" });
+export const edit = (req, res) => res.render("edit", { pageTitle: "Edit" });
 export const deleteVideo = (req, res) => res.send("Delete Video");
 export const search = (req, res) => res.send("Search Video");
 export const upload = (req, res) => res.send("Upload Video");
