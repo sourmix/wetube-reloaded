07023ca88908b2b8474764166fbbf9ab28496898
sourmix <75124027+sourmix@users.noreply.github.com>
2022-07-26 10:08:44 +0900

    pug2

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,4 +1,32 @@
-export const trending = (req, res) => res.render("home", { pageTitle: "Home" });
+export const trending = (req, res) => {
+  const videos = [
+    {
+      title: "first Video",
+      rating: 5,
+      comments: 2,
+      createdAt: "2 minutes age",
+      views: 59,
+      id: 1,
+    },
+    {
+      title: "second Video",
+      rating: 5,
+      comments: 2,
+      createdAt: "2 minutes age",
+      views: 59,
+      id: 1,
+    },
+    {
+      title: "third Video",
+      rating: 5,
+      comments: 2,
+      createdAt: "2 minutes age",
+      views: 59,
+      id: 1,
+    },
+  ];
+  return res.render("home", { pageTitle: "Home", videos });
+};
 export const see = (req, res) => res.render("watch", { pageTitle: "Watch" });
 export const edit = (req, res) => res.render("edit", { pageTitle: "Edit" });
 export const deleteVideo = (req, res) => res.send("Delete Video");
